52ed1f98d5f8591dacd3234e8051d91457ea51ab
davidbachmann <dabbi@dabbi.is>
2021-10-17 21:22:36 +0000

    Update colorscheme

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -5,10 +5,16 @@ vim.opt.guifont = "Hack Nerd Font Mono:h15"
 -- Use , instead of leader for bkad/CamelCaseMotion
 vim.g.camelcasemotion_key = ','
 
+-- We need an annoyingly long timeout for 'surround' movements
+vim.o.timeoutlen = 800
+
+-- Make sure Lightspeed's keymaps are set.
+-- I had issues where native vim 'S' took over Lightspeed's.
+vim.api.nvim_set_keymap('n', 's', '<Plug>Lightspeed_s', {})
+vim.api.nvim_set_keymap('n', 'S', '<Plug>Lightspeed_S', {})
+
 -- Disable mouse clicks
 vim.opt.mouse = ""
-
-lvim.lsp.diagnostics.virtual_text = true
 
 lvim.builtin.dashboard.custom_header = {
 "                                                           ",
@@ -27,7 +33,7 @@ lvim.builtin.dashboard.custom_header = {
 -- general
 lvim.log.level = "warn"
 lvim.format_on_save = true
-lvim.colorscheme = "nightfly"
+lvim.colorscheme = "punk"
 
 -- keymappings [view all the defaults by pressing <leader>Lk]
 lvim.leader = "space"
@@ -36,8 +42,8 @@ lvim.keys.normal_mode = {
   ["<Tab>"] = "<C-w>", -- Tab is the same as pressing ctrl-w
   ["ScrollWheelUp"] = "<Nop>", -- disable mouse scroll
   ["ScrollWheelDown"] = "<Nop>", -- disable mouse scroll
-  ["<S-h>"] = "<cmd>BufferPrevious<cr>",
-  ["<S-l>"] = "<cmd>BufferNext<cr>",
+  ["<S-h>"] = "<cmd>BufferPrevious<cr>", -- Switch buffers
+  ["<S-l>"] = "<cmd>BufferNext<cr>", -- Switch buffers
 }
 
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
@@ -48,34 +54,34 @@ lvim.builtin.nvimtree.show_icons.git = 1
 lvim.builtin.nvimtree.setup = {
   view = {
     mappings = {
-      custom_only = true,
-      list = {
-        { key = {"<CR>", "o"},                  cb = tree_cb("edit") },
-        { key = "s",                            cb = tree_cb("vsplit") },
-        { key = "<C-t>",                        cb = tree_cb("tabnew") },
-        { key = "<",                            cb = tree_cb("prev_sibling") },
-        { key = ">",                            cb = tree_cb("next_sibling") },
-        { key = "P",                            cb = tree_cb("parent_node") },
-        { key = "<BS>",                         cb = tree_cb("close_node") },
-        { key = "<S-CR>",                       cb = tree_cb("close_node") },
-        { key = "K",                            cb = tree_cb("first_sibling") },
-        { key = "J",                            cb = tree_cb("last_sibling") },
-        { key = "I",                            cb = tree_cb("toggle_ignored") },
-        { key = "H",                            cb = tree_cb("toggle_dotfiles") },
-        { key = "R",                            cb = tree_cb("refresh") },
-        { key = "a",                            cb = tree_cb("create") },
-        { key = "d",                            cb = tree_cb("remove") },
-        { key = "r",                            cb = tree_cb("rename") },
-        { key = "<C-r>",                        cb = tree_cb("full_rename") },
-        { key = "x",                            cb = tree_cb("cut") },
-        { key = "c",                            cb = tree_cb("copy") },
-        { key = "p",                            cb = tree_cb("paste") },
-        { key = "y",                            cb = tree_cb("copy_name") },
-        { key = "Y",                            cb = tree_cb("copy_path") },
-        { key = "gy",                           cb = tree_cb("copy_absolute_path") },
-        { key = "-",                            cb = tree_cb("dir_up") },
-        { key = "q",                            cb = tree_cb("close") },
-        { key = "g?",                           cb = tree_cb("toggle_help") },
+      custom_only = true, -- To remap one key you have to opt out of all defaults.
+      list = { -- And then set them again.
+        { key = {"<CR>", "o"}, cb = tree_cb("edit") },
+        { key = "s",           cb = tree_cb("vsplit") },
+        { key = "<C-t>",       cb = tree_cb("tabnew") },
+        { key = "<",           cb = tree_cb("prev_sibling") },
+        { key = ">",           cb = tree_cb("next_sibling") },
+        { key = "P",           cb = tree_cb("parent_node") },
+        { key = "<BS>",        cb = tree_cb("close_node") },
+        { key = "<S-CR>",      cb = tree_cb("close_node") },
+        { key = "K",           cb = tree_cb("first_sibling") },
+        { key = "J",           cb = tree_cb("last_sibling") },
+        { key = "I",           cb = tree_cb("toggle_ignored") },
+        { key = "H",           cb = tree_cb("toggle_dotfiles") },
+        { key = "R",           cb = tree_cb("refresh") },
+        { key = "a",           cb = tree_cb("create") },
+        { key = "d",           cb = tree_cb("remove") },
+        { key = "r",           cb = tree_cb("rename") },
+        { key = "<C-r>",       cb = tree_cb("full_rename") },
+        { key = "x",           cb = tree_cb("cut") },
+        { key = "c",           cb = tree_cb("copy") },
+        { key = "p",           cb = tree_cb("paste") },
+        { key = "y",           cb = tree_cb("copy_name") },
+        { key = "Y",           cb = tree_cb("copy_path") },
+        { key = "gy",          cb = tree_cb("copy_absolute_path") },
+        { key = "-",           cb = tree_cb("dir_up") },
+        { key = "q",           cb = tree_cb("close") },
+        { key = "g?",          cb = tree_cb("toggle_help") },
       }
     }
   }
@@ -101,18 +107,23 @@ lvim.builtin.treesitter.highlight.enabled = true
 lvim.builtin.treesitter.ensure_installed = "maintained"
 
 -- Configure builtin plugins
-lvim.builtin.dashboard.active = true
 lvim.builtin.terminal.active = true
 
-lvim.lsp.diagnostics.virtual_text = true
+--lvim.lsp.diagnostics.virtual_text = true
 
 -- Additional Plugins
 lvim.plugins = {
   {
-    "bluz71/vim-nightfly-guicolors"
+    "davidbachmann/vim-punk-colorscheme"
+  },
+  {
+    "ggandor/lightspeed.nvim",
   },
   {
     "psliwka/vim-smoothie"
+  },
+  {
+    "tpope/vim-surround",
   },
   {
   "norcalli/nvim-colorizer.lua",
@@ -141,36 +152,10 @@ lvim.plugins = {
     end
   },
   {
-    "tpope/vim-surround",
-     keys = {"c", "d", "y"}
-  },
-  {
     "bkad/CamelCaseMotion"
   },
   {
     "folke/trouble.nvim",
       cmd = "TroubleToggle",
   },
-  "ggandor/lightspeed.nvim",
-  setup = function()
-    require'lightspeed'.setup {
-      jump_to_first_match = true,
-      jump_on_partial_input_safety_timeout = 400,
-      -- This can get _really_ slow if the window has a lot of content,
-      -- turn it on only if your machine can always cope with it.
-      highlight_unique_chars = false,
-      grey_out_search_area = true,
-      match_only_the_start_of_same_char_seqs = true,
-      limit_ft_matches = 5,
-      x_mode_prefix_key = '<c-x>',
-      substitute_chars = { ['\r'] = '¬' },
-      instant_repeat_fwd_key = nil,
-      instant_repeat_bwd_key = nil,
-      -- If no values are given, these will be set at runtime,
-      -- based on `jump_to_first_match`.
-      labels = nil,
-      cycle_group_fwd_key = nil,
-      cycle_group_bwd_key = nil,
-    }
-  end
 }
