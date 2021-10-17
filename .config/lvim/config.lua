71b385421fd035b450a49d3769f6a3fde32094ee
davidbachmann <dabbi@dabbi.is>
2021-10-17 09:28:46 +0000

    Rebind nvim-tree

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -1,3 +1,5 @@
+local tree_cb = require'nvim-tree.config'.nvim_tree_callback
+
 vim.opt.relativenumber = true -- set relative numbered lines
 vim.opt.guifont = "Hack Nerd Font Mono:h15"
 -- Use , instead of leader for bkad/CamelCaseMotion
@@ -9,13 +11,17 @@ vim.opt.mouse = ""
 lvim.lsp.diagnostics.virtual_text = true
 
 lvim.builtin.dashboard.custom_header = {
-" :::::::::  :::::::::  :::::::::::    ::::::::::: ::::::::  ",
-" :+:    :+: :+:    :+:     :+:            :+:    :+:    :+: ",
-" +:+    +:+ +:+    +:+     +:+            +:+    +:+        ",
-" +#+    +:+ +#++:++#+      +#+            +#+    +#++:++#++ ",
-" +#+    +#+ +#+    +#+     +#+            +#+           +#+ ",
-" #+#    #+# #+#    #+# #+# #+#   #+#      #+#    #+#    #+# ",
-" #########  #########   #####    ###  ########### ########  ",
+"                                                           ",
+"                                                           ",
+" :::::::::  :::::::::  :::::::::::   ::::::::::: ::::::::  ",
+" :+:    :+: :+:    :+:     :+:           :+:    :+:    :+: ",
+" +:+    +:+ +:+    +:+     +:+           +:+    +:+        ",
+" +#+    +:+ +#++:++#+      +#+           +#+    +#++:++#++ ",
+" +#+    +#+ +#+    +#+     +#+           +#+           +#+ ",
+" #+#    #+# #+#    #+# #+# #+#  #+#      #+#    #+#    #+# ",
+" #########  #########   #####   ###  ########### ########  ",
+"                                                           ",
+"                                                           ",
 }
 
 -- general
@@ -30,6 +36,8 @@ lvim.keys.normal_mode = {
   ["<Tab>"] = "<C-w>", -- Tab is the same as pressing ctrl-w
   ["ScrollWheelUp"] = "<Nop>", -- disable mouse scroll
   ["ScrollWheelDown"] = "<Nop>", -- disable mouse scroll
+  ["<S-h>"] = "<cmd>BufferPrevious<cr>",
+  ["<S-l>"] = "<cmd>BufferNext<cr>",
 }
 
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
@@ -37,6 +45,41 @@ lvim.builtin.dashboard.active = true
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.setup.view.side = "left"
 lvim.builtin.nvimtree.show_icons.git = 1
+lvim.builtin.nvimtree.setup = {
+  view = {
+    mappings = {
+      custom_only = true,
+      list = {
+        { key = {"<CR>", "o"},                  cb = tree_cb("edit") },
+        { key = "s",                            cb = tree_cb("vsplit") },
+        { key = "<C-t>",                        cb = tree_cb("tabnew") },
+        { key = "<",                            cb = tree_cb("prev_sibling") },
+        { key = ">",                            cb = tree_cb("next_sibling") },
+        { key = "P",                            cb = tree_cb("parent_node") },
+        { key = "<BS>",                         cb = tree_cb("close_node") },
+        { key = "<S-CR>",                       cb = tree_cb("close_node") },
+        { key = "K",                            cb = tree_cb("first_sibling") },
+        { key = "J",                            cb = tree_cb("last_sibling") },
+        { key = "I",                            cb = tree_cb("toggle_ignored") },
+        { key = "H",                            cb = tree_cb("toggle_dotfiles") },
+        { key = "R",                            cb = tree_cb("refresh") },
+        { key = "a",                            cb = tree_cb("create") },
+        { key = "d",                            cb = tree_cb("remove") },
+        { key = "r",                            cb = tree_cb("rename") },
+        { key = "<C-r>",                        cb = tree_cb("full_rename") },
+        { key = "x",                            cb = tree_cb("cut") },
+        { key = "c",                            cb = tree_cb("copy") },
+        { key = "p",                            cb = tree_cb("paste") },
+        { key = "y",                            cb = tree_cb("copy_name") },
+        { key = "Y",                            cb = tree_cb("copy_path") },
+        { key = "gy",                           cb = tree_cb("copy_absolute_path") },
+        { key = "-",                            cb = tree_cb("dir_up") },
+        { key = "q",                            cb = tree_cb("close") },
+        { key = "g?",                           cb = tree_cb("toggle_help") },
+      }
+    }
+  }
+}
 
 -- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = {
