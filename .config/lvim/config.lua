d0cbf07ec9c7897bda66e398824fc286d7e002ea
davidbachmann <dabbi@dabbi.is>
2022-04-23 19:21:36 +0000

    Make lvim config work for nvim 0.7 and tidy things up

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -1,44 +1,6 @@
-local tree_cb = require'nvim-tree.config'.nvim_tree_callback
-require"fidget".setup{}
-
-vim.opt.relativenumber = true -- set relative numbered lines
-vim.opt.guifont = "Hack Nerd Font Mono:h15"
-
--- Use , instead of leader for bkad/CamelCaseMotion
-vim.g.camelcasemotion_key = ','
-
--- Close tags automatically
-vim.g.closetag_filenames = '*.tsx,*.jsx,*.html'
-
--- We need an annoyingly long timeout for 'surround' movements
-vim.o.timeoutlen = 800
-
--- Enable Blamer for git blames
-vim.g.blamer_enabled = 1
-vim.g.blamer_show_in_insert_modes = 0
-vim.g.blamer_date_format = '%d/%m/%y'
-
--- Close quickfix list on item select
-vim.api.nvim_command('autocmd FileType qf nnoremap <buffer> <CR> <CR>:cclose<CR>')
-
--- Make sure Lightspeed's keymaps are set.
--- I had issues where native vim 'S' took over Lightspeed's.
-vim.api.nvim_set_keymap('n', 's', '<Plug>Lightspeed_s', {})
-vim.api.nvim_set_keymap('n', 'S', '<Plug>Lightspeed_S', {})
-
--- Neovide thinks I mean ∆ when I type A-j
-vim.api.nvim_set_keymap('n', '∆', ":m .+1<CR>==", {})
-vim.api.nvim_set_keymap('v', '∆', ":m '>+1<CR>gv=gv", {})
-
--- Neovide thinks I mean ˚ when I type A-k
-vim.api.nvim_set_keymap('n', '˚', ':m .-2<CR>==', {})
-vim.api.nvim_set_keymap('v', '˚', ":m '<-2<CR>gv=gv", {})
-
--- Disable mouse
-vim.opt.mouse = ""
-
 -- Null-ls configuration
 local null_ls = require("null-ls")
+null_ls.setup({ sources = sources })
 
 -- Register null-ls sources
 local sources = {
@@ -47,28 +9,49 @@ local sources = {
     null_ls.builtins.code_actions.gitsigns,
 }
 
-null_ls.setup({ sources = sources })
+vim.o.cmdheight = 1
 
-
--- show hidden files when running a find command in telescope
-lvim.builtin.telescope.pickers = { find_files = { hidden = true } }
+vim.opt.guifont = "Hack Nerd Font Mono:h15"
+vim.opt.termguicolors = true
+vim.opt.mouse = ""
 
 -- general
 lvim.log.level = "warn"
 lvim.format_on_save = true
 lvim.colorscheme = "punk"
 
+-- Enable Blamer for git blames
+vim.g.blamer_enabled = 1
+vim.g.blamer_show_in_insert_modes = 0
+vim.g.blamer_date_format = '%d/%m/%y'
+
+-- Use , instead of leader for bkad/CamelCaseMotion
+vim.g.camelcasemotion_key = ','
+
 -- keymappings [view all the defaults by pressing <leader>Lk]
 lvim.leader = "space"
 lvim.keys.normal_mode = {
   ["<esc>"] = "<cmd>nohlsearch<cr>", -- Turn off highlight after search
-  ["<Tab>"] = "<C-w>", -- Tab is the same as pressing ctrl-w
-  ["<Tab><Tab>"] = "<C-w>w", -- ctrl-w-w
+  ["<tab>"] = "<C-w>", -- Tab is the same as pressing ctrl-w
+  ["<tab><tab>"] = "<C-w>w", -- ctrl-w-w
   ["ScrollWheelUp"] = "<Nop>", -- disable mouse scroll
   ["ScrollWheelDown"] = "<Nop>", -- disable mouse scroll
   ["<S-h>"] = "<cmd>BufferLineCyclePrev<cr>", -- Switch buffers
   ["<S-l>"] = "<cmd>BufferLineCycleNext<cr>", -- Switch buffers
 }
+
+-- keymappings that WhichKey should manage and display
+lvim.builtin.which_key.mappings["E"] = {
+  name = "eslint_d",
+  f = {"mF:%!eslint_d --stdin --fix-to-stdout --stdin-filename %<CR>`F", "Fix buffer"},
+}
+
+lvim.builtin.alpha.active = true
+lvim.builtin.alpha.mode = "dashboard"
+lvim.builtin.notify.active = true
+lvim.builtin.terminal.active = true
+lvim.builtin.nvimtree.setup.view.side = "left"
+lvim.builtin.nvimtree.show_icons.git = 0
 
 -- I actually need to type kj and jk on occation
 lvim.keys.insert_mode['jj'] = false
@@ -90,57 +73,38 @@ lvim.builtin.lualine.sections.lualine_b = {
   }
 }
 
-lvim.builtin.terminal.active = true
-lvim.builtin.nvimtree.show_icons.git = 1
+-- if you don't want all the parsers change this to a table of the ones you want
+lvim.builtin.treesitter.ensure_installed = {
+  "bash",
+  "c",
+  "javascript",
+  "json",
+  "python",
+  "typescript",
+  "tsx",
+  "css",
+  "rust",
+  "java",
+  "yaml",
+}
+
+lvim.builtin.treesitter.ignore_install = { "haskell" }
+lvim.builtin.treesitter.highlight.enabled = true
+
 lvim.builtin.nvimtree.setup = {
   view = {
     width = 36,
     side = "left",
     auto_resize = true,
     mappings = {
-      custom_only = true, -- To remap one key you have to opt out of all defaults.
-      list = { -- And then set them again.
-        { key = {"<CR>", "o"}, cb = tree_cb("edit") },
-        { key = "s",           cb = tree_cb("vsplit") },
-        { key = "<C-t>",       cb = tree_cb("tabnew") },
-        { key = "<",           cb = tree_cb("prev_sibling") },
-        { key = ">",           cb = tree_cb("next_sibling") },
-        { key = "P",           cb = tree_cb("parent_node") },
-        { key = "<BS>",        cb = tree_cb("close_node") },
-        { key = "<S-CR>",      cb = tree_cb("close_node") },
-        { key = "K",           cb = tree_cb("first_sibling") },
-        { key = "J",           cb = tree_cb("last_sibling") },
-        { key = "I",           cb = tree_cb("toggle_ignored") },
-        { key = "H",           cb = tree_cb("toggle_dotfiles") },
-        { key = "R",           cb = tree_cb("refresh") },
-        { key = "a",           cb = tree_cb("create") },
-        { key = "d",           cb = tree_cb("remove") },
-        { key = "r",           cb = tree_cb("rename") },
-        { key = "<C-r>",       cb = tree_cb("full_rename") },
-        { key = "x",           cb = tree_cb("cut") },
-        { key = "c",           cb = tree_cb("copy") },
-        { key = "p",           cb = tree_cb("paste") },
-        { key = "y",           cb = tree_cb("copy_name") },
-        { key = "Y",           cb = tree_cb("copy_path") },
-        { key = "gy",          cb = tree_cb("copy_absolute_path") },
-        { key = "-",           cb = tree_cb("dir_up") },
-        { key = "q",           cb = tree_cb("close") },
-        { key = "g?",          cb = tree_cb("toggle_help") },
+      list = {
+        { key = "<Tab>", action = "", }
       }
     }
   },
   update_focused_file = {
     enable = true
   }
-}
-
-lvim.builtin.treesitter.highlight.enabled = true
--- Treesitter parsers change this to a table of the languages you want i.e. {"java", "python", javascript}
-lvim.builtin.treesitter.ensure_installed = "maintained"
-
-lvim.builtin.treesitter.incremental_selection = {
-  enable = true,
-  keymaps = { init_selection = '<CR>', scope_incremental = '<CR>', node_incremental = '<TAB>', node_decremental = '<S-TAB>' }
 }
 
 
@@ -156,21 +120,10 @@ lvim.plugins = {
     "psliwka/vim-smoothie"
   },
   {
-    "tpope/vim-surround",
-  },
-  {
-  "norcalli/nvim-colorizer.lua",
-    config = function()
-      require("colorizer").setup({ "*" }, {
-        RGB = true, -- #RGB hex codes
-        RRGGBB = true, -- #RRGGBB hex codes
-        RRGGBBAA = true, -- #RRGGBBAA hex codes
-        rgb_fn = true, -- CSS rgb() and rgba() functions
-        hsl_fn = true, -- CSS hsl() and hsla() functions
-        css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
-        css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
-      })
-    end,
+    "ur4ltz/surround.nvim",
+      config = function()
+        require"surround".setup {mappings_style = "surround"}
+      end
   },
   {
     "lukas-reineke/indent-blankline.nvim",
@@ -189,8 +142,5 @@ lvim.plugins = {
   },
   {
     "APZelos/blamer.nvim"
-  },
-  {
-    'j-hui/fidget.nvim'
-  },
+  }
 }
