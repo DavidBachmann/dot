3507915bc53e64800d243f791f665ff7059918ca
David Bachmann <dabbi@dabbi.is>
2022-04-27 21:04:18 +0000

    Fix null-ls sources being used before being defined...

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -1,6 +1,5 @@
 -- Null-ls configuration
 local null_ls = require("null-ls")
-null_ls.setup({ sources = sources })
 
 -- Register null-ls sources
 local sources = {
@@ -8,6 +7,8 @@ local sources = {
     null_ls.builtins.diagnostics.eslint_d,
     null_ls.builtins.code_actions.gitsigns,
 }
+
+null_ls.setup({ sources = sources })
 
 vim.o.cmdheight = 1
 
