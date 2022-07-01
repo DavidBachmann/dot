3e68bf9a3cc23995b5a0d6e13af0835224fdb064
davidbachmann <dabbi@dabbi.is>
2022-07-01 10:30:20 +0000

    New lvim config

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -1,6 +1,4 @@
 -- Null-ls configuration
-local null_ls = require("null-ls")
-
 -- Register null-ls sources
 require("null-ls").setup({
     sources = {
@@ -9,47 +7,54 @@ require("null-ls").setup({
     },
 })
 
-null_ls.setup({ sources = sources })
-
 vim.o.cmdheight = 1
-
-vim.opt.guifont = "Hack Nerd Font Mono:h15"
-vim.opt.termguicolors = true
-vim.opt.mouse = ""
 
 -- general
 lvim.log.level = "warn"
 lvim.format_on_save = true
 lvim.colorscheme = "tokyonight"
 
--- Enable Blamer for git blames
-vim.g.blamer_enabled = 1
-vim.g.blamer_show_in_insert_modes = 0
-vim.g.blamer_date_format = '%d/%m/%y'
-
--- Use , instead of leader for bkad/CamelCaseMotion
+vim.opt.guifont = "Hack Nerd Font Mono:h15"
 vim.g.camelcasemotion_key = ','
 
 -- keymappings [view all the defaults by pressing <leader>Lk]
 lvim.leader = "space"
-lvim.keys.normal_mode = {
-  ["<esc>"] = "<cmd>nohlsearch<cr>", -- Turn off highlight after search
-  ["<Tab>"] = "<C-w>", -- Tab is the same as pressing ctrl-w
-  ["<Tab><Tab>"] = "<C-w>w", -- ctrl-w-w
-  ["ScrollWheelUp"] = "<Nop>", -- disable mouse scroll
-  ["ScrollWheelDown"] = "<Nop>", -- disable mouse scroll
-  ['S'] = '<Plug>Lightspeed_S' -- Sneak search backwards
+-- add your own keymapping
+
+lvim.keys.insert_mode["˚"] = "<Esc>:m .-2<CR>==gi"
+lvim.keys.insert_mode["∆"] = "<Esc>:m .+1<CR>==gi"
+
+lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
+lvim.keys.normal_mode["<esc>"] = "<cmd>nohlsearch<cr>" -- Turn off highlight after search
+
+-- Switch line positions up/down
+lvim.keys.normal_mode["˚"] = "<Esc>:m .-2<CR>=="
+lvim.keys.normal_mode["∆"] = "<Esc>:m .+1<CR>=="
+lvim.keys.normal_mode["<A-k>"] = "<Esc>:m .-2<CR>=="
+lvim.keys.normal_mode["<A-j>"] = "<Esc>:m .+1<CR>=="
+
+-- Switch to window left/right
+lvim.keys.normal_mode["˙"] = "<C-w>h"
+lvim.keys.normal_mode["¬"] = "<C-w>l"
+lvim.keys.normal_mode["<A-h>"] = "<C-w>h"
+lvim.keys.normal_mode["<A-k>"] = "<C-w>k"
+
+-- Use which-key to add extra bindings with the leader-key prefix
+lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
+lvim.builtin.which_key.mappings["t"] = {
+  name = "+Trouble",
+  r = { "<cmd>Trouble lsp_references<cr>", "References" },
+  f = { "<cmd>Trouble lsp_definitions<cr>", "Definitions" },
+  d = { "<cmd>Trouble document_diagnostics<cr>", "Diagnostics" },
+  q = { "<cmd>Trouble quickfix<cr>", "QuickFix" },
+  l = { "<cmd>Trouble loclist<cr>", "LocationList" },
+  w = { "<cmd>Trouble workspace_diagnostics<cr>", "Wordspace Diagnostics" },
 }
-
-
--- keymappings that WhichKey should manage and display
 lvim.builtin.which_key.mappings["E"] = {
   name = "eslint_d",
   f = {"mF:%!eslint_d --stdin --fix-to-stdout --stdin-filename %<CR>`F", "Fix buffer"},
 }
 
-lvim.builtin.alpha.active = true
-lvim.builtin.alpha.mode = "dashboard"
 
 -- I actually need to type kj and jk on occation
 lvim.keys.insert_mode['jj'] = false
@@ -62,37 +67,108 @@ lvim.keys.visual_block_mode['J'] = false
 -- I like K for lsp.hover
 lvim.keys.visual_block_mode['K'] = false
 
--- Show filename and path in lualine
-lvim.builtin.lualine.sections.lualine_b = {
-  {
-    'filename',
-    file_status = true, -- displays file status (readonly status, modified status)
-    path = 1 -- 0 = just filename, 1 = relative path, 2 = absolute path
-  }
-}
-
-
+-- TODO: User Config for predefined plugins
+-- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
+lvim.builtin.alpha.active = true
+lvim.builtin.alpha.mode = "dashboard"
+lvim.builtin.notify.active = true
+lvim.builtin.terminal.active = true
+lvim.builtin.nvimtree.setup.view.side = "left"
 
 -- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = {
   "bash",
+  "c",
   "javascript",
   "json",
+  "lua",
+  "python",
   "typescript",
   "tsx",
   "css",
+  "rust",
+  "java",
   "yaml",
 }
 
 lvim.builtin.treesitter.ignore_install = { "haskell" }
 lvim.builtin.treesitter.highlight.enabled = true
 
+-- generic LSP settings
 
-vim.g.indentLine_enabled = 1
-vim.g.indent_blankline_filetype_exclude = {"help", "terminal", "dashboard"}
-vim.g.indent_blankline_buftype_exclude = {"terminal"}
-vim.g.indent_blankline_show_trailing_blankline_indent = false
-vim.g.indent_blankline_show_first_indent_level = false
+-- ---@usage disable automatic installation of servers
+-- lvim.lsp.automatic_servers_installation = false
+
+-- ---configure a server manually. !!Requires `:LvimCacheReset` to take effect!!
+-- ---see the full default list `:lua print(vim.inspect(lvim.lsp.automatic_configuration.skipped_servers))`
+-- vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "pyright" })
+-- local opts = {} -- check the lspconfig documentation for a list of all possible options
+-- require("lvim.lsp.manager").setup("pyright", opts)
+
+-- ---remove a server from the skipped list, e.g. eslint, or emmet_ls. !!Requires `:LvimCacheReset` to take effect!!
+-- ---`:LvimInfo` lists which server(s) are skiipped for the current filetype
+-- vim.tbl_map(function(server)
+--   return server ~= "emmet_ls"
+-- end, lvim.lsp.automatic_configuration.skipped_servers)
+
+-- -- you can set a custom on_attach function that will be used for all the language servers
+-- -- See <https://github.com/neovim/nvim-lspconfig#keybindings-and-completion>
+-- lvim.lsp.on_attach_callback = function(client, bufnr)
+--   local function buf_set_option(...)
+--     vim.api.nvim_buf_set_option(bufnr, ...)
+--   end
+--   --Enable completion triggered by <c-x><c-o>
+--   buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
+-- end
+
+-- -- set a formatter, this will override the language server formatting capabilities (if it exists)
+-- local formatters = require "lvim.lsp.null-ls.formatters"
+-- formatters.setup {
+--   { command = "black", filetypes = { "python" } },
+--   { command = "isort", filetypes = { "python" } },
+--   {
+--     -- each formatter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
+--     command = "prettier",
+--     ---@usage arguments to pass to the formatter
+--     -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
+--     extra_args = { "--print-with", "100" },
+--     ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
+--     filetypes = { "typescript", "typescriptreact" },
+--   },
+-- }
+
+-- -- set additional linters
+-- local linters = require "lvim.lsp.null-ls.linters"
+-- linters.setup {
+--   { command = "flake8", filetypes = { "python" } },
+--   {
+--     -- each linter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
+--     command = "shellcheck",
+--     ---@usage arguments to pass to the formatter
+--     -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
+--     extra_args = { "--severity", "warning" },
+--   },
+--   {
+--     command = "codespell",
+--     ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
+--     filetypes = { "javascript", "python" },
+--   },
+-- }
+
+-- Autocommands (https://neovim.io/doc/user/autocmd.html)
+-- vim.api.nvim_create_autocmd("BufEnter", {
+--   pattern = { "*.json", "*.jsonc" },
+--   -- enable wrap mode for json files only
+--   command = "setlocal wrap",
+-- })
+-- vim.api.nvim_create_autocmd("FileType", {
+--   pattern = "zsh",
+--   callback = function()
+--     -- let treesitter use bash highlight for zsh files as well
+--     require("nvim-treesitter.highlight").attach(0, "bash")
+--   end,
+-- })
+
 
 -- Additional Plugins
 lvim.plugins = {
@@ -125,5 +201,9 @@ lvim.plugins = {
   },
   {
     "APZelos/blamer.nvim"
-  }
+  },
+  {
+    "folke/trouble.nvim",
+    cmd = "TroubleToggle",
+  },
 }
