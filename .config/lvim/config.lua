3e3aa12debe444368c288439da7424de17f81691
Davíð Bachmann <dabbi@dabbi.is>
2023-06-18 10:23:41 +0000

    Update config.lua

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -1,44 +1,19 @@
--- Null-ls configuration
--- Register null-ls sources
-
-local null_ls = require("null-ls")
-
-null_ls.setup({
-  sources = {
-    require("null-ls").builtins.formatting.prettierd,
-    require("null-ls").builtins.diagnostics.eslint_d,
-  }
-})
-
-
-
 vim.o.cmdheight = 1
 vim.opt.timeoutlen = 1000
 vim.o.updatetime = 250
-
--- Disable virtual text disagnostics
-lvim.lsp.diagnostics.virtual_text = false
-lvim.lsp.diagnostics.underline = true
--- and enable floating diagnostics
-vim.cmd [[autocmd! CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]]
-
--- general
-lvim.log.level = "warn"
-lvim.format_on_save = true
-lvim.colorscheme = "tokyonight"
-
+vim.diagnostic.config({
+  virtual_text = false,
+  underline = true
+})
 vim.opt.guifont = "Hack Nerd Font Mono:h15"
 vim.g.camelcasemotion_key = ','
 
--- keymappings [view all the defaults by pressing <leader>Lk]
+-- general
+lvim.format_on_save = true
+lvim.colorscheme = "tokyonight"
 lvim.leader = "space"
 
-lvim.keys.insert_mode["˚"] = "<Esc>:m .-2<CR>==gi"
-lvim.keys.insert_mode["∆"] = "<Esc>:m .+1<CR>==gi"
-
-lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 lvim.keys.normal_mode["<esc>"] = "<cmd>nohlsearch<cr>" -- Turn off highlight after search
-
 lvim.keys.normal_mode["<S-h>"] = "<cmd>BufferLineCyclePrev<cr>"
 lvim.keys.normal_mode["<S-l>"] = "<cmd>BufferLineCycleNext<cr>"
 
@@ -46,31 +21,23 @@ lvim.keys.normal_mode["<S-l>"] = "<cmd>BufferLineCycleNext<cr>"
 lvim.keys.normal_mode["˚"] = "<Esc>:m .-2<CR>=="
 lvim.keys.normal_mode["∆"] = "<Esc>:m .+1<CR>=="
 lvim.keys.normal_mode["<A-k>"] = "<Esc>:m .-2<CR>=="
-lvim.keys.normal_mode["<A-j>"] = "<Esc>:m .+1<CR>=="
-
--- Switch to window left/right
-lvim.keys.normal_mode["˙"] = "<C-w>h"
-lvim.keys.normal_mode["¬"] = "<C-w>l"
-lvim.keys.normal_mode["<A-h>"] = "<C-w>h"
-lvim.keys.normal_mode["<A-k>"] = "<C-w>k"
+lvim.keys.normal_mode["<A-j>"] = "<Esc>:m .1<CR>=="
 
 -- Use which-key to add extra bindings with the leader-key prefix
 lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
 lvim.builtin.which_key.mappings["t"] = {
-  name = "+Trouble",
+  name = "Trouble",
   r = { "<cmd>Trouble lsp_references<cr>", "References" },
   f = { "<cmd>Trouble lsp_definitions<cr>", "Definitions" },
   d = { "<cmd>Trouble document_diagnostics<cr>", "Diagnostics" },
   q = { "<cmd>Trouble quickfix<cr>", "QuickFix" },
   l = { "<cmd>Trouble loclist<cr>", "LocationList" },
-  w = { "<cmd>Trouble workspace_diagnostics<cr>", "Wordspace Diagnostics" },
-}
-lvim.builtin.which_key.mappings["E"] = {
-  name = "eslint_d",
-  f = { "mF:%!eslint_d --stdin --fix-to-stdout --stdin-filename %<CR>`F", "Fix buffer" },
+  w = { "<cmd>Trouble workspace_diagnostics<cr>", "Wordspace Diagnostics" }
 }
 
-lvim.builtin.which_key.mappings["C"] = { "<cmd>NoNeckPain<CR>", "Center/NoNeckPain.nvim" }
+lvim.builtin.which_key.mappings["sT"] = { "<cmd>Telescope live_grep search_dirs=.<CR>", "Search Local Fext" }
+lvim.builtin.which_key.mappings["F"] = { "<cmd>Telescope find_files search_dirs=.<CR>", "Find Local Files" }
+lvim.builtin.which_key.mappings["bs"] = { "<cmd>Scratch<CR>", "Open Scratch" }
 
 
 -- I actually need to type kj and jk on occation
@@ -83,14 +50,6 @@ lvim.keys.visual_block_mode['J'] = false
 
 -- I like K for lsp.hover
 lvim.keys.visual_block_mode['K'] = false
-
--- TODO: User Config for predefined plugins
--- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
-lvim.builtin.alpha.active = true
-lvim.builtin.alpha.mode = "dashboard"
-lvim.builtin.terminal.active = true
-lvim.builtin.nvimtree.setup.view.side = "left"
-lvim.builtin.nvimtree.setup.view.adaptive_size = true
 
 -- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = {
@@ -132,7 +91,6 @@ lvim.plugins = {
   },
   {
     "kylechui/nvim-surround",
-    tag = "*", -- Use for stability; omit to use `main` branch for the latest features
     config = function()
       require("nvim-surround").setup({
         -- Configuration here, or leave empty to use defaults
@@ -140,53 +98,16 @@ lvim.plugins = {
     end
   },
   {
-    "shortcuts/no-neck-pain.nvim",
+    'wfxr/minimap.vim',
+    build = "cargo install --locked code-minimap",
+    -- cmd = {"Minimap", "MinimapClose", "MinimapToggle", "MinimapRefresh", "MinimapUpdateHighlight"},
     config = function()
-      require("no-neck-pain").setup({
-        -- The width of the focused buffer when enabling NNP.
-        -- If the available window size is less than `width`, the buffer will take the whole screen.
-        width = 143,
-        -- Prints useful logs about what event are triggered, and reasons actions are executed.
-        debug = false,
-        -- Disables NNP if the last valid buffer in the list has been closed.
-        disableOnLastBuffer = false,
-        -- When `true`, disabling NNP kills every split/vsplit buffers except the main NNP buffer.
-        killAllBuffersOnDisable = false,
-        -- Options related to the side buffers.
-        buffers = {
-          -- The background options of the side buffer(s).
-          background = {
-            colorCode = nil,
-          },
-          -- When `false`, the `left` padding buffer won't be created.
-          left = true,
-          -- When `false`, the `right` padding buffer won't be created.
-          right = true,
-          -- When `true`, the side buffers will be named `no-neck-pain-left` and `no-neck-pain-right` respectively.
-          showName = false,
-          -- The buffer options when creating the buffer.
-          options = {
-            -- Buffer-scoped options, below are the default values, but any `vim.bo` options are valid and will be forwarded to the buffer creation.
-            bo = {
-              filetype = "no-neck-pain",
-              buftype = "nofile",
-              bufhidden = "hide",
-              modifiable = false,
-              buflisted = false,
-              swapfile = false,
-            },
-            -- Window-scoped options, below are the default values, but any `vim.wo` options are valid and will be forwarded to the buffer creation.
-            wo = {
-              cursorline = false,
-              cursorcolumn = false,
-              number = false,
-              relativenumber = false,
-              foldenable = false,
-              list = false,
-            },
-          },
-        },
-      })
-    end
+      vim.cmd("let g:minimap_width = 10")
+      vim.cmd("let g:minimap_auto_start = 1")
+      vim.cmd("let g:minimap_auto_start_win_enter = 1")
+    end,
+  },
+  {
+    'mtth/scratch.vim'
   }
 }
